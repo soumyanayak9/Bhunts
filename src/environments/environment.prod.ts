70dab863dac4139278ef811ef8b1ed6c7ca2b039
soumyanayak9 <82140737+soumyanayak9@users.noreply.github.com>
2021-05-29 16:45:54 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+
+export const environment = {
+  production: true,
+  baseURL: 'http://demo-grocery-api.softhubspot.com/index.php/',
+  mediaURL: 'http://demo-grocery-api.softhubspot.com/uploads/',
+  onesignal: {
+    appId: 'YOUR_APP_ID',
+    googleProjectNumber: 'YOUR_PROJECT_ID',
+    restKey: 'YOUR_PROJECT_REST_KEY'
+  },
+  general: {
+    symbol: '$',
+    code: 'USD'
+  },
+  authToken: '987654321'
+};
