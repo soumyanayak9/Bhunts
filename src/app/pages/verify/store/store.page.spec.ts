5564e3daaacd691d60000216af3db87ffb759056
soumyanayak9 <82140737+soumyanayak9@users.noreply.github.com>
2021-05-29 17:01:05 +0530

    Add files via upload

---
+++ src/app/pages/verify/store/store.page.spec.ts
@@ -0,0 +1,25 @@
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { StorePage } from './store.page';
+
+describe('StorePage', () => {
+  let component: StorePage;
+  let fixture: ComponentFixture<StorePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [StorePage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StorePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
