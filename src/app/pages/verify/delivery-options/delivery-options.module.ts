5621d514ec25e69320f9a1ea3a0a5f10d7b30285
soumyanayak9 <82140737+soumyanayak9@users.noreply.github.com>
2021-05-29 17:06:26 +0530

    Add files via upload

---
+++ src/app/pages/verify/delivery-options/delivery-options.module.ts
@@ -0,0 +1,23 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DeliveryOptionsPageRoutingModule } from './delivery-options-routing.module';
+
+import { DeliveryOptionsPage } from './delivery-options.page';
+import { ComponentsModule } from 'src/app/components/components.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    DeliveryOptionsPageRoutingModule,
+    ComponentsModule
+  ],
+  declarations: [DeliveryOptionsPage]
+})
+export class DeliveryOptionsPageModule { }
---
+++ src/app/pages/verify/favorite/favorite.module.ts
@@ -0,0 +1,21 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { FavoritePageRoutingModule } from './favorite-routing.module';
+
+import { FavoritePage } from './favorite.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    FavoritePageRoutingModule
+  ],
+  declarations: [FavoritePage]
+})
+export class FavoritePageModule { }
